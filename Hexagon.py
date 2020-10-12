77c81eef48049c54a255b4f267cea90988e6df44
AidanOrefice <59771180+AidanOrefice@users.noreply.github.com>
2020-10-12 11:01:34 +0100

    Update Hexagon.py

--- Hexagon.py
+++ Hexagon.py
@@ -1,1 +1,5 @@
 #Script to initialise the hexagonal lattice
+
+#Essentially will be a network with each site having neighbours - apart from boundary cases.
+
+#Need to be able to randomly remove bonds between certain sites and track which sites are bonded.
